02fd605db16a5197cb329f68941e81ae7107f045
iamrajendra <rajendrarajaram@hotmail.com>
2020-09-06 16:04:15 +0530

    Make a initial Database connection with Firebase.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,20 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
 
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBp-2rtFbW-fuZ2I2vH4wU4FyChaU7SY4U",
+    authDomain: "tour-of-heros-7b08c.firebaseapp.com",
+    databaseURL: "https://tour-of-heros-7b08c.firebaseio.com",
+    projectId: "tour-of-heros-7b08c",
+    storageBucket: "tour-of-heros-7b08c.appspot.com",
+    messagingSenderId: "210960868157",
+    appId: "1:210960868157:web:0e6f08b0fdbef401fb9b54",
+    measurementId: "G-9274H90MJ3"
+  }
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
